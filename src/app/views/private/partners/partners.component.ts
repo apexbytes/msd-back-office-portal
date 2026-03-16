de620e31ad5641745dce471ab33365c238c1d473
dave-budah <davebudah@gmail.com>
2026-03-16 16:33:45 +0200

    Partners connected to delete dialog.

--- src/app/views/private/partners/partners.component.ts
+++ src/app/views/private/partners/partners.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { TitleCasePipe } from '@angular/common';
 import { PartnerFormComponent } from '@app/views/private/forms/partner-form/partner-form.component';
 import { DeleteDialogComponent } from '@app/views/shared/delete-dialog/delete-dialog.component';
-import { DeleteDialogData } from '@app/core/models/delete.interface';
 import { PartnerService, QueryParams } from '@app/core/services/partner.service';
+import { LoadingService } from '@app/core/services/loading.service';
 import { Partner } from '@app/core/models/partner.model';
-import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-partners',
@@ -18,6 +18,7 @@ import { TitleCasePipe } from '@angular/common';
 export class PartnersComponent implements OnInit {
   private readonly dialog = inject(MatDialog);
   private readonly partnerService = inject(PartnerService);
+  private readonly loadingService = inject(LoadingService);
 
   protected readonly partners = signal<Partner[]>([]);
   protected readonly totalCount = signal(0);
@@ -81,20 +82,34 @@ export class PartnersComponent implements OnInit {
 
   protected onDelete(partner: Partner): void {
     const dialogRef = this.dialog.open(DeleteDialogComponent, {
-      data: {
-        entityName: 'Partner',
-        title: 'Delete Partner?',
-        message:
-          'Are you sure you want to delete "' + partner.name + '"? This process is permanent.',
-        deleteFn: () => this.partnerService.deletePartner(partner.id),
-      } as DeleteDialogData,
+      width: '540px',
+      maxWidth: '95vw',
       disableClose: true,
-      maxWidth: '500px',
-      width: '90%',
+      panelClass: 'full-screen-modal',
+      data: {
+        title: 'Delete Partner',
+        message: `Are you sure you want to delete the partner "${partner.name}"? This process is permanent and cannot be undone.`,
+        itemType: 'Partner',
+        itemName: partner.name,
+      },
     });
 
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result) this.fetchPartners();
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.loadingService.show();
+        this.partnerService.deletePartner(partner.id).subscribe({
+          next: (response) => {
+            if (response.success) {
+              this.fetchPartners();
+            }
+            this.loadingService.hide();
+          },
+          error: (err) => {
+            console.error(`Error deleting partner: ${partner.name}`, err);
+            this.loadingService.hide();
+          },
+        });
+      }
     });
   }
 }
