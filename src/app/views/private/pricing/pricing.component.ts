331757a9cb33d7dd72b88179af47998308a7a5f0
dave-budah <davebudah@gmail.com>
2026-03-17 09:04:32 +0200

    Vehicle models and makes dialogs updated.

--- src/app/views/private/pricing/pricing.component.ts
+++ src/app/views/private/pricing/pricing.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-help',
-  imports: [],
+  selector: 'app-pricing',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.css',
 })
